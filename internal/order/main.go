7a4b433a0f4c2a01eb4781de294cab359075be88
5Asuka“ <ZhangFeng72@163.com>
2025-08-08 00:32:26 +0800

    http基本接口搭建

--- internal/order/main.go
+++ internal/order/main.go
@@ -2,24 +2,19 @@ package main
 
 import (
 	"log"
-	"net/http"
+
+	"github.com/5Asuka/order/common/config"
+	"github.com/spf13/viper"
 )
+
+func init() {
+	if err := config.NewViperConfig(); err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
 
-	log.Println("Starting server... Listening on :8082")
+	log.Printf("%v", viper.Get("order"))
 
-	//创建相应页面
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%v\n", r.RequestURI)
-		_, _ = w.Write([]byte("pong"))
-	})
-
-	//启动页面 错误处理
-	err := http.ListenAndServe(":8082", mux)
-	if err != nil {
-		log.Fatal(err)
-	}
 }
